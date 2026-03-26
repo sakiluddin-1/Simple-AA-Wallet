d3b5888e016aaa3d366a3d6362d53e3fad11f780
sakiluddin-1 <md.001449@snuindia.in>
2026-03-26 11:50:20 +0530

    added script

--- script/SimpleWallet.s.sol
+++ script/SimpleWallet.s.sol
@@ -3,19 +3,26 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
 import "../src/SimpleWallet.sol";
+import "openzepplin-contracts/contracts/utils/cryptography/ECDSA.sol";
 
 contract DeploySimpleWallet is Script {
     function run() public {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        address owner = vm.addr(privateKey);
 
-        vm.startBroadcast();
-
-        address owner = vm.addr(deployerPrivateKey);
+        vm.startBroadcast(privateKey);
 
         SimpleWallet wallet = new SimpleWallet(owner);
+        bytes32 hash = keccak256(abi.encodePacked("test-operation"));
+        bytes32 userOpHash = ECDSA.toEthSignedMessageHash(hash);
 
-        console.log("Contract deployed at: ", address(wallet));
-        console.log("Owner: ", owner);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, userOpHash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+
+        //wallet.validateUserOp(userOpHash, signature);
+
+        console.log("UserOp validated successfully");
 
         vm.stopBroadcast();
     }
