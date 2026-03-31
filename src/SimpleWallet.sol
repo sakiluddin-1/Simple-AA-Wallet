266f702161ca91edaa561d705be09d9446ab7597
sakiluddin-1 <md.001449@snuindia.in>
2026-03-31 13:10:52 +0530

    simple entrypoint logic

--- src/SimpleWallet.sol
+++ src/SimpleWallet.sol
@@ -1,36 +1,39 @@
 // SPDX-License-Identifier: MIt
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.30;
 
 import "lib/openzepplin-contracts/contracts/utils/cryptography/ECDSA.sol";
+import "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
 contract SimpleWallet {
-
     using ECDSA for bytes32;
 
     address public owner;
     uint256 public nonce;
+    IEntryPoint public entryPoint;
 
-    constructor(address _owner) {
+    constructor(address _owner, address _entryPoint) {
         owner = _owner;
+        entryPoint = IEntryPoint(_entryPoint);
     }
 
-    function validateUserOp(bytes32 userOpHash, bytes calldata signature) external returns(bool) {
-        address signer = userOpHash.toEthSignedMessageHash().recover(signature);
+    modifier onlyEntryPoint() {
+        require(msg.sender == address(entryPoint), "Not EntryPoint");
+        _;
+    }
 
-        require(owner == signer, "You are not the owner");
+    function validateUSerOp(bytes32 userOpHash, bytes calldata signature) external onlyEntryPoint returns(bool) {
+        address signer = userOpHash.toEthSignedMessageHash().recover(signature);
+        require(signer == owner, "You are not the owner");
 
         nonce ++;
 
         return true;
     }
 
-    function execute(address _to, uint256 _value, bytes calldata data) external {
-        require(msg.sender == address(this), "Only contract can execute");
+    function execute(address _to, uint256 _value, bytes calldata _data) external onlyEntryPoint {
+        require(msg.sender == address(this), "Only self call is allowed");
 
-        (bool success, ) = _to.call{value : _value}(data);
-        require(success, "Tx failed");
+        (bool success, ) = _to.call{value: _value}(_data);
+        require(success, "Transaction failed");
     }
-
-    receive() external payable{}
-    fallback() external payable{}
 }
